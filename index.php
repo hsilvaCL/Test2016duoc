fd66e47ffd2fbf4931fea6cc637b2c8025865b10
cetecom <cetecom@LC08L1300>
2016-03-16 20:15:37 -0300

    Nuevas versiones

--- index.php
+++ index.php
@@ -1,19 +1,51 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+﻿<?php
+ $titulo=null;
+ $a=0;
+ $b=5;
+?>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        /*Que buen programador soy, no necesito IDE!!!!!*/
-        /*Otro cambio*/
-        // put your code here
-        ?>
-    </body>
+<meta charset="utf8">
+<title><?=$titulo;?></title>
+<link type="text/css" rel="stylesheet" href="estilo.css" >
+<body>
+
+<?php
+/*
+Primer codigo de prueba
+*/
+if(isset($titulo)){
+if($titulo!=null){
+	echo "Hola Mundo";
+}
+else{
+	echo "Algo sucedio";
+}
+}
+
+$a=$b;
+$b++;
+
+echo "El valor de la variable a=$a y el de b=$b";
+?>
+<ul>
+<?php
+for($i=0;$i<$b;$i++){
+	echo "<li>Hola mundo $i</li>\n";
+}
+?>
+</ul>
+
+<h1>OTRA FORMA DE HACER LO MISMO</h1>
+
+<ul>
+
+<?php for($i=0;$i<$b;$i++){ ?>
+<li><?="Hola mundo $i\n";}?></li>
+<?php}?>
+</ul>
+<h1>VINCULOS</h1>
+<h2> TAG "a"</h2>
+<a href="pagina1.php">Link a mi página 01</a>
+<a href="pagina2.php">Link a mi página 02</a>
+</body>
 </html>
